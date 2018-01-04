7ae54dd903f38a9b89197107335aa8af6ed02de0
Minori <mimimi0611@icloud.com>
2018-01-04 18:23:32 +0900

    Simulatorが真っ黒。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。。

--- photo/editPhotoViewController.swift
+++ photo/editPhotoViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Social
 
 class editPhotoViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
@@ -17,7 +18,12 @@ class editPhotoViewController: UIViewController, UIImagePickerControllerDelegate
     var brightness = 1.0
     var contrast = 1.0
     let slider = UISlider(frame: CGRect(x:0, y:0, width:300, height:30))
+    private var twitterButton: UIButton!
+    private var facebookButton: UIButton!
+    private var instagramButton: UIButton!
     private var button: UIButton!
+    var composeView: SLComposeViewController!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,7 +31,6 @@ class editPhotoViewController: UIViewController, UIImagePickerControllerDelegate
     }
     
     func addSlider() {
-        let slider = UISlider(frame: CGRect(x:0, y:0, width:300, height:30))
         slider.layer.position = CGPoint(x:self.view.frame.midX, y:600)
         slider.layer.cornerRadius = 10.0
         slider.layer.shadowOpacity = 0.5
@@ -47,7 +52,7 @@ class editPhotoViewController: UIViewController, UIImagePickerControllerDelegate
         button.frame = CGRect(x: posX, y: posY, width: bWidth, height: bHeight)
         button.setTitle("Done", for: .normal)
         button.setTitleColor(UIColor.white, for: .normal)
-        //button.addTarget(self, action: #selector(editPhotoViewController.removeSlider(sender:)), for: .touchUpInside)
+        button.addTarget(self, action: #selector(self.removeSlider(sender:)), for: .touchUpInside)
         self.view.addSubview(button)
     }
     
@@ -70,16 +75,15 @@ class editPhotoViewController: UIViewController, UIImagePickerControllerDelegate
     }
     
 //スライダーの削除
-    func removeSlider() {
-            slider.removeFromSuperview()
-//            button.removeFromSuperview()
+    func removeSlider(sender: UIButton) {
+        slider.removeFromSuperview()
+        button.removeFromSuperview()
         print("here")
     }
     
     @IBAction func brightnessButton() {
         addSlider()
         addButton()
-
         slider.addTarget(self, action: #selector(self.changeBrightness(sender:)), for: .valueChanged)
         self.view.addSubview(slider)
         
@@ -120,6 +124,52 @@ class editPhotoViewController: UIViewController, UIImagePickerControllerDelegate
         let ctx = CIContext(options: nil)
         let cgImage = ctx.createCGImage(filter.outputImage!, from: filter.outputImage!.extent)
         imageView.image = UIImage(cgImage: cgImage!)
+    }
+    
+    @IBAction func sharePhoto(){
+        addShareButtons()
+    }
+    
+    func addShareButtons() {
+        //twitter
+        twitterButton.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        twitterButton.setTitle("Twitter", for: UIControlState.normal)
+        twitterButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        twitterButton.layer.position = CGPoint(x: self.view.frame.width/2, y:self.view.frame.height/2)
+        twitterButton.addTarget(self, action: #selector(self.postonTwitter(sender:)), for: .touchUpInside)
+        
+        self.view.addSubview(twitterButton)
+        
+        //facebook
+        facebookButton.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        facebookButton.setTitle("Facebook", for: UIControlState.normal)
+        facebookButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        facebookButton.layer.position = CGPoint(x: self.view.frame.width/2, y:self.view.frame.height/2)
+        facebookButton.addTarget(self, action: #selector(self.postonFacebook(sender:)), for: .touchUpInside)
+        
+        self.view.addSubview(facebookButton)
+        
+        //instagram
+        instagramButton.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        instagramButton.setTitle("Instagram", for: UIControlState.normal)
+        instagramButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+        instagramButton.layer.position = CGPoint(x: self.view.frame.width/2, y:self.view.frame.height/2)
+        instagramButton.addTarget(self, action: #selector(self.postonInstagram(sender:)), for: .touchUpInside)
+        
+        self.view.addSubview(facebookButton)
+    }
+    
+    func postonTwitter(sender: UIButton) {
+        composeView = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        composeView.add(imageView.image!)
+    }
+    
+    func postonFacebook(sender: UIButton) {
+        composeView = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+        composeView.add(imageView.image!)
+    }
+    
+    func postonInstagram(sender: UIButton) {
     }
     
     @IBAction func savePhoto() {
